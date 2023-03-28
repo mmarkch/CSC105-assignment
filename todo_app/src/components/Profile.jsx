ba8c73d8fc3954b332081216e83657268c2d22cb
mmarkch <mark110447@gmail.com>
2023-03-28 14:44:22 +0700

    Lab

---
+++ todo_app/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+function Profile (){
+    return(
+        <div className="layout">
+            <img src="assets/pizzaaaaa.jpg" alt="profile"/>
+            <h1 className="name">Mark Mananchai</h1>
+            <hr></hr>
+        </div>
+    )
+}
+export default Profile;
